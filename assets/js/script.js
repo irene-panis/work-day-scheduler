99f0aa11a065fdbc7adfe5b1a05a284c4682c844
irene <panisirene@gmail.com>
2023-05-03 01:09:45 -0700

    Add func to style time blocks based on time plus interval func

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,26 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+  // loops through timeblocks and colors them based on current time
+  function colorTimeBlocks() {
+    $( '.time-block' ).each(function() {
+      var hour = this.id.replace('hour-', '');
+      if (dayjs().hour() > hour) {
+        $( this ).removeClass( 'future present' ).addClass( 'past' );
+      } else if (dayjs().hour() < hour) {
+        $( this ).removeClass( 'past present' ).addClass( 'future' );
+      } else {
+        $( this ).removeClass( 'future past' ).addClass( 'present' );
+      }
+    });
+  }
+  
+  // sets interval to run colorTimeBlocks and display current date in header every 60 seconds to account for time changes
+  setInterval(() => {
+    colorTimeBlocks();
+    $( '#currentDay' ).text(dayjs().format('dddd - MMMM D, YYYY')); // displays current date and refreshes every min
+  }, 60000); // re-color time blocks every minute (checks for hour changes)
+  colorTimeBlocks(); // initialize time blocks
   $( '#currentDay' ).text(dayjs().format('dddd - MMMM D, YYYY')); // init current date
 });
